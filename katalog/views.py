5892eca4dcde7890e31ce40330cd47321f3f3080
sherlockjack <sherlocknagata@gmail.com>
2022-09-13 10:57:32 +0700

    Tugas 1 PBP

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_barang_katalog,
+    'Name': 'Reiou Nagata',
+    'ID': '2106636943'
+    }
+    return render(request, "katalog.html", context)
+    #return render(request, "katalog.html")
